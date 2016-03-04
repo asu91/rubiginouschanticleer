095615c7c3851164101d5b2e45f6ab93472c1d4a
Rhombus33 <diamond.d.wheeler@gmail.com>
2016-03-03 19:23:57 -0800

    resolve merge conflict from rebase

--- client/app/movieinput/movieinput.js
+++ client/app/movieinput/movieinput.js
@@ -1,6 +1,10 @@
 angular.module('moviematch.movieinput', [])
 
+<<<<<<< Updated upstream
 .controller('MovieInputController', function($scope, $http, $location, Session, Lobby, $uibModalInstance) {
+=======
+.controller('MovieInputController', function($scope, $http, requestFactory, Session) {
+>>>>>>> Stashed changes
   $scope.movieTitle = "";
   $scope.searchResults;
   $scope.movieChoices = [];
@@ -8,27 +12,26 @@ angular.module('moviematch.movieinput', [])
   $scope.session = {};
   $scope.users;
 
-  $scope.fetchSearch = function() {
-    $http({
-      method: 'GET',
-      url: '/api/omdb/' + $scope.movieTitle
-    }).then(function(res){
+  // Get movies and assigns them to $scope.searchResults
+  $scope.fetchSearchResults = function() {
+    requestFactory.omdb($scope.movieTitle)
+      .then(function(res){
+        var data = JSON.parse(res.data);
+        $scope.searchResults = data.Search;
+      });
+  };
+
+  // Get movies and assigns them to $scope.searchResults
+  $scope.fetchInTheaters = function() {
+    requestFactory.movieDB()
+      .then(function(res){
         var results = JSON.parse(res.data);
         $scope.searchResults = results.Search;
-    });
-  }
+      });
+  };
 
-  // TODO
-  // $scope.fetchCollection = function() {
-  //   $http({
-  //     method: 'GET',
-  //     url: '/api/movieDB/' + $scope.movieTitle
-  //   }).then(function(res){
-  //     // console.log(res.data);
-  //      // $scope.searchResults = JSON.parse(res.data);
-  //   });
-  // }
-
+  // Adds and removes movies from $scope.movieChoices
+  // Assigns error messages if > 5 movies chosen
   $scope.updateMovies = function(add, movie) {
     if (add && $scope.movieChoices.length < 5 ) {
       $scope.movieChoices.push(movie);
@@ -38,6 +41,18 @@ angular.module('moviematch.movieinput', [])
     } else {
       $scope.error = "Already 5 movies!!";
     }
+  };
+
+  // Fired when user completes choices
+  $scope.readyToVote = function($http) {
+    var session_name = Session.getSession();
+    var movies = 
+    var options = {
+      session_name: session_name,
+      movies: $scope.movieChoices
+    };
+    requestFactory.addMovies(options);
+    $uibModalInstance.close($scope.movieChoices);
   }
 
   $scope.$watch('movieTitle', $scope.fetchSearch);
@@ -56,13 +71,38 @@ angular.module('moviematch.movieinput', [])
     $location.path('/loading')
   }
 
-
-
-  $scope.ok = function () {
-    $uibModalInstance.close($scope.movieChoices);
-  };
-
   $scope.cancel = function () {
     $uibModalInstance.dismiss('cancel');
+  };
+})
+
+.factory('requestFactory', function($http) {
+  // Makes request to omdb API and returns results in a promise
+  var omdb = function(movieTitle) {
+    return $http({
+      method: 'GET',
+      url: '/api/omdb/' + movieTitle
+    });
+  };
+  // Makes request to movieDB API and returns results in a promise
+  var movieDB = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/movieDB/'
+    });
+  };
+  // Makes request to add movies to database and returns nothing
+  var addMovies = function(data) {
+    return $http({
+      method: 'POST',
+      url: '/api/moviesTODO',
+      data: data
+    });
+  };
+
+  return {
+    omdb: omdb,
+    movieDB: movieDB,
+    addMovies: addMovies
   };
 });
